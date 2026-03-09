bf62a4b3f87df0ad8a4c1eaf24822eb6ec18048f
diegorichi <diegorichi@gmail.com>
2026-03-09 13:48:36 -0300

    import directo

--- futbol.py
+++ futbol.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import unicodedata
 import re
 import time
 import sys
@@ -17,13 +18,38 @@ M3U_FILE = os.getenv("M3U_FILE")
 THREADFIN_API_URL = os.getenv("THREADFIN_API_URL", "http://localhost:34400/api/")
 SINTEL_URL = "https://demo.unified-streaming.com/k8s/live/scte35.isml/.m3u8"
 
-def es_horario_valido(hora_str):
+def sanitizar_nombre(texto):
+    if not texto:
+        return ""
+    # 1. Normalizar Unicode (convierte caracteres raros a su forma base)
+    texto = unicodedata.normalize('NFKC', texto)
+
+    # 2. Reemplazar saltos de línea, retornos y tabs por un espacio simple
+    texto = re.sub(r'[\r\n\t]+', ' ', texto)
+    # 3. Quitar espacios múltiples y dejar solo uno
+    texto = re.sub(r'\s+', ' ', texto)
+
+    # 4. Limpiar los bordes
+    return texto.strip()
+
+def es_proximo(hora_str):
     try:
         ahora = datetime.now()
         hora_obj = datetime.strptime(hora_str, "%H:%M").replace(
             year=ahora.year, month=ahora.month, day=ahora.day
         )
-        # Eventos activos: desde hace 2.5 horas hasta 5 mins en el futuro
+        # Eventos activos: desde hace 2.5 horas hasta 30 mins en el futuro
+        return (ahora + timedelta(minutes=30)) <= hora_obj
+    except:
+        return False
+
+def es_activo(hora_str):
+    try:
+        ahora = datetime.now()
+        hora_obj = datetime.strptime(hora_str, "%H:%M").replace(
+            year=ahora.year, month=ahora.month, day=ahora.day
+        )
+        # Eventos activos: desde hace 2.5 horas hasta 30 mins en el futuro
         return (ahora - timedelta(hours=2, minutes=30)) <= hora_obj <= (ahora + timedelta(minutes=30))
     except:
         return False
@@ -45,6 +71,7 @@ def extraer_todo_futbol_libre():
             return Array.from(document.querySelectorAll('#menu > li')).map(li => ({
                 nombre: li.querySelector('div span') ? li.querySelector('div span').textContent.trim() : "",
                 hora: li.querySelector('div div time') ? li.querySelector('div div time').textContent.trim() : "00:00",
+                logo: li.querySelector('div div img').src, // Ruta del logo
                 opciones: Array.from(li.querySelectorAll('ul a')).map(a => ({
                     url: a.href,
                     canal: a.querySelector('span') ? a.querySelector('span').textContent.trim() : "Opción"
@@ -57,12 +84,13 @@ def extraer_todo_futbol_libre():
         proximos = []
         
         for ev in eventos_raw:
-            if es_horario_valido(ev['hora']):
+            if es_activo(ev['hora']):
                 for opt in ev['opciones']:
-                    en_vivo.append({'nombre': ev['nombre'], 'hora': ev['hora'], 'canal': opt['canal'], 'url': opt['url']})
+                    en_vivo.append({'nombre': ev['nombre'], 'hora': ev['hora'], 'canal': opt['canal'], 'logo': ev['logo'], 'url': opt['url']})
             else:
-                # Guardamos los proximos para rellenar si sobran slots
-                proximos.append(ev)
+                if (es_proximo(ev['hora'])):
+                    # Guardamos los proximos para rellenar si sobran slots
+                    proximos.append(ev)
 
         m3u_content = "#EXTM3U\n"
         
@@ -73,7 +101,8 @@ def extraer_todo_futbol_libre():
             if len(en_vivo) > 0:
                 # Ocupar slot con evento en vivo
                 item = en_vivo.pop(0)
-                print(f"Slot {slot_id}: {item['nombre']} ({item['canal']})")
+                nombre = sanitizar_nombre(item['nombre'])
+                print(f"Slot {slot_id}: {nombre} ({item['canal']})")
                 
                 try:
                     driver.get(item['url'])
@@ -83,28 +112,30 @@ def extraer_todo_futbol_libre():
                     
                     if match:
                         link_stream = match.group(1)
-                        nombre_txt = f"[{item['hora']}] {item['nombre']} - {item['canal']}"
+                        nombre_txt = f"[{item['hora']}] {nombre} - {item['canal']}"
                     else:
                         link_stream = SINTEL_URL
-                        nombre_txt = f"[{item['hora']}] {item['nombre']} (Link no encontrado)"
+                        nombre_txt = f"[{item['hora']}] {nombre} (Link no encontrado)"
                 except:
                     link_stream = SINTEL_URL
-                    nombre_txt = f"[{item['hora']}] {item['nombre']} (Error de carga)"
+                    nombre_txt = f"[{item['hora']}] {nombre} (Error de carga)"
                 
                 driver.switch_to.default_content()
             else:
                 # Rellenar con "Próximamente"
                 if len(proximos) > 0:
                     px = proximos.pop(0)
-                    nombre_txt = f"PROXIMAMENTE: [{px['hora']}] {px['nombre']}"
+                    nombre = sanitizar_nombre(px['nombre'])
+
+                    nombre_txt = f"PROXIMAMENTE: [{px['hora']}] {nombre}"
                 else:
                     nombre_txt = "Slot Libre - Sin Eventos"
                 
                 link_stream = SINTEL_URL
                 print(f"Slot {slot_id}: {nombre_txt}")
-
+            logo = item['logo']
             # Escribir el canal al M3U (siempre con el mismo tvg-id para la tele)
-            m3u_content += f'#EXTINF:-1 tvg-id="{slot_id}" tvg-name="Evento {i}" group-title="EVENTOS", {nombre_txt}\n'
+            m3u_content += f'#EXTINF:-1 tvg-id="{slot_id}" tvg-name="Evento {i}" tvg-logo="{logo}" group-title="Sports", {nombre_txt}\n'
             m3u_content += f'#EXTVLCOPT:http-user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"\n'
             m3u_content += f'{link_stream}\n'
 
